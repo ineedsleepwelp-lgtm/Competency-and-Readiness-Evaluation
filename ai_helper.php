07b19b21c3574744e9659ba0d09fadc5a00104b6
ineedsleepwelp-lgtm <ineedsleepwelp@gmail.com>
2026-05-24 01:16:17 +0800

    Update ai_helper.php

--- ai_helper.php
+++ ai_helper.php
@@ -6,7 +6,7 @@ function generateAIResponse($context_text, $mode = 'evaluator', $pdf_base64 = nu
         return "System Error: The cURL extension is not enabled in your XAMPP php.ini file.";
     }
 
-    $apiKey = "AIzaSyBlD0zkrfGDTSNIWIMorEHMqX4ySNAgisc"; 
+    $apiKey = getenv('GEMINI_API_KEY'); 
     $apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" . $apiKey;
 
     if ($mode == 'evaluator') {
